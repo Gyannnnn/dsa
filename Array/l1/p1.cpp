3e612d2364fcfbf64862942baa127f3b294d8466
gyanaranjan <hi.gyanaranjanpatra@gmail.com>
2025-08-08 14:15:01 +0530

    largest el

---
+++ Array/l1/p1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int size;
+    cin >> size;
+    int arr[size];
+    int max = 0;
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+    cout << max;
+    return 0;
+}
---
+++ Array/l1/p2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    int arr[6]  = {1,2,4,7,7,5};
+    int max1 = 0,max2 = 0;
+    for(int i = 0;i<6;i++){
+        
+    
+    }
+    return 0;
+}
